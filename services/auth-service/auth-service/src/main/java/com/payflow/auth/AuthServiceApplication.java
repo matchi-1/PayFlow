89ebef0a21bb2cc7c7f1a7cea29701b6bf503be5
matchi-1 <misheruvynx@gmail.com>
2026-05-08 15:00:01 +0800

    refactored dir for auth-service

--- services/auth-service/auth-service/src/main/java/com/payflow/auth/AuthServiceApplication.java
+++ services/auth-service/auth-service/src/main/java/com/payflow/auth/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.authservice;
+package com.payflow.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/auth-service/auth-service/src/test/java/com/payflow/auth/AuthServiceApplicationTests.java
+++ services/auth-service/auth-service/src/test/java/com/payflow/auth/AuthServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.authservice;
+package com.payflow.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
